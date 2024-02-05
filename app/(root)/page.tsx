cb1f8c3e75a6604c88ee4082363d3381beb2cfd7
Joao Oliveira <jaoliveira98@gmail.com>
2024-02-05 23:10:26 +0000

    Add User Settings After Sign In

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <p>Admin Dashboard</p>;
-}
+import { UserButton } from "@clerk/nextjs";
+
+const SetupPage = () => {
+  return (
+    <>
+      <UserButton showName />
+    </>
+  );
+};
+
+export default SetupPage;
