547334e92a57fa2ba4f9ad494e1687ffdf2c7c40
Joao Oliveira <jaoliveira98@gmail.com>
2024-02-06 18:24:02 +0000

    Prisma, PlanetScale, MySQL setup

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,9 +2,12 @@
 
 import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { useState } from "react";
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const [loading, setLoading] = useState(false);
 
   return (
     <Modal
